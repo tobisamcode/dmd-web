6fc1140dba7ffae7541282dee01ef5e1a509fbb0
tobidev <samueloluwatobiloba48@gmail.com>
2023-07-18 19:31:28 +0100

    implement hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,36 @@
 import { BiRightArrowAlt } from "react-icons/bi";
 
 import Button from "components/Button";
+import Nav from "components/Nav";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Page() {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
+  const handleMenuToggle = () => {
+    setShowMenu((prevState) => !prevState);
+  };
+
   return (
     <>
-      <div className="app">
-        <div>
+      <div className="app ">
+        <div className="">
           <header className="w-full mb-[40px] py-[1.875rem] px-[4.375vw] lg:px-[11.029vw] flex justify-between md:shadow-header-shadow bg-[#FAFAFA] border-b border-[rgba(191, 191, 191, 0.20)] md:border-none md:bg-white items-center ">
-            <div className="text-black-black_10 font-helvetica text-[24px] md:text-[1.75em] font-[700] ">
-              DMD <span className="font-[400] -ml-2">.design</span>
-            </div>
+            <Link href="/">
+              <div className="text-black-black_10 font-helvetica text-[24px] md:text-[1.75em] font-[700] ">
+                DMD <span className="font-[400] -ml-2">.design</span>
+              </div>
+            </Link>
 
-            <div className="flex gap-x-[1.923vw]">
+            <div className="flex gap-x-[1.923vw] cursor-pointer">
               <img
                 src="svg/darkmodeicon.svg"
                 className="w-[1.5rem] md:w-[2rem]"
                 alt="menu"
               />
               <img
+                onClick={handleMenuToggle}
                 src="svg/menuicon.svg"
                 className="w-[1.5rem] md:w-[2rem]"
                 alt="dark mode"
@@ -26,16 +38,28 @@ export default function Page() {
             </div>
           </header>
 
-          <main className="  px-[5vw] md:px-[11.029vw]">
-            <div className="grid  md:grid-cols-2 md:gap-x-[150px] ">
-              <div className="md:py-[3.625rem] flex flex-col md:justify-center">
-                <span className="flex text-black-black_9 font-[500] text-[18px] lg:text-[1.5rem] mb-[24px]">
+          <>
+            {showMenu && (
+              <div
+                className={`overlay ${showMenu === true ? "active" : ""}`}
+                onClick={handleMenuToggle}
+              ></div>
+            )}
+            <Nav showMenu={showMenu} toggleMenu={handleMenuToggle} />
+          </>
+
+          <main className="  px-[5vw] md:px-[11.029vw] relative bg-hero-pattern-1 bg-no-repeat md:bg-none overflow-hidden ">
+            <div className="hidden md:flex absolute w-[640px] h-[640px] rounded-full border border-[#EBEBEB] -left-[150px] top-52"></div>
+            <div className="hidden md:flex md:absolute  w-[640px] h-[640px] rounded-full border border-[#EBEBEB] left-24 top-52"></div>
+            <div className=" grid  md:grid-cols-2 md:gap-x-[150px] bg-hero-pattern-2 bg-no-repeat md:bg-none ">
+              <div className="md:py-[3.625rem] flex flex-col md:justify-center md:font-[500]">
+                <span className="flex text-black-black_9  text-[18px] lg:text-[1.5rem] mb-[24px]">
                   <h1 className="mr-[4px]">Hi</h1>
                   <img src="svg/hiicon.svg" alt="hi" />
                   <h1>,I’m Moses Denapo</h1>
                 </span>
 
-                <p className="md:text-[20px] text-[18px] lg:text-[32px] lg:w-[510px] font-[500] text-black-black_9 mb-[32px]">
+                <p className=" md:text-[20px] text-[18px] lg:text-[32px] lg:w-[510px]  text-black-black_9 mb-[32px]">
                   A creative digital product designer skilled in finding the
                   sweet spots between what users want to make their lives easier
                   and what the business needs to accomplish in order to survive
@@ -50,7 +74,7 @@ export default function Page() {
                   />
                   <Button
                     text="Read more about me"
-                    className="py-[0.75rem] md:py-[1rem]  w-[260px] flex items-center gap-x-[8px] px-[24px] md:px-[32px] rounded-[4px] text-[18px] font-[400] text-black-black_11 border border-black-black_2"
+                    className="py-[0.75rem] md:py-[1rem]  w-[265px] flex items-center gap-x-[8px] px-[24px] md:px-[32px] rounded-[4px] text-[18px] font-[400] text-black-black_11 border border-black-black_2"
                     icon={<img src="svg/arrowrighticon.svg" />}
                   />
                 </div>
