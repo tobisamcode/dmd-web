2a21783639ceca93936370ae94cec4a7ee865688
tobidev <samueloluwatobiloba48@gmail.com>
2023-07-25 02:25:31 +0100

    add a new section

---
+++ data-store.tsx
@@ -0,0 +1,25 @@
+export const projectData = [
+  {
+    image: "/img/project1.png",
+    title: "A UX Case Study on CakeVilla’s responsive website",
+    desc: "The goal of this project was to design a responsive website for a Cake producing company  that allows users to easily order cakes and get the cakes delivered to their locations.",
+  },
+
+  {
+    image: "/img/project2.png",
+    title: "FinTrack UX Case Study",
+    desc: "FinTrack is a mobile app for managing personal finances. The app allows users to link their bank accounts, track their spending, set budget goals, and get alerts when they are approaching their budget limits",
+  },
+
+  {
+    image: "/img/project3.png",
+    title: "Craigslist Website Redesign",
+    desc: "The goal of this project was to modernize the user interface of Craigslist, improve usability and keep the overall user experience consistent so that long time visitors don’t lose touch of the website",
+  },
+
+  {
+    image: "/img/project4.png",
+    title: "Pricing plan page and Checkout flow (DWR Challenge) ",
+    desc: "My entry for the design with Rooks challenge. The task was to design a pricing plan page and a check-out flow that accepts credit card and crypto payment",
+  },
+];
