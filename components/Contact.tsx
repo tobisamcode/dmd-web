6a1ca6ac242f37320cec3edc834dc52c4ce80dd3
tobidev <samueloluwatobiloba48@gmail.com>
2023-08-19 18:29:33 +0100

    dark mode

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,19 +1,27 @@
 import Link from "next/link";
 import React from "react";
+import { useTheme } from "next-themes";
 
 const Contact = () => {
+  const { theme, setTheme } = useTheme();
+
   return (
     <>
-      <div className=" bg-contact-pattern " id="contact-me">
-        <main className="container">
+      <div
+        className={`${
+          theme === "light" ? "bg-contact-pattern" : ""
+        }  dark:bg-project_bg `}
+        id="contact-me"
+      >
+        <main className="container  ">
           <div className="py-[120px] md:py-[180px]  ">
-            <p className="text-black-black_10 font-[700]  text-[1.75rem] lg:text-[48px] font-helvetica mb-4 md:mb-[32px] ">
+            <p className="text-black-black_10 dark:text-white-white font-[700]  text-[1.75rem] lg:text-[48px] font-helvetica mb-4 md:mb-[32px] ">
               Your Product Vision, Crafted to Perfection! 🌟 Reach Out, let's
               Collaborate and design brilliance
             </p>
             <Link
               href="mosesdenapo@gmail.com"
-              className="text-black-black_8 font-[500]  underline text-[1rem] lg:text-[32px]"
+              className="text-black-black_8 dark:text-white-white_8 font-[500]  underline text-[1rem] lg:text-[32px]"
             >
               send me a mail📩
             </Link>
