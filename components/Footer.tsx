72d3ee70c1a22206ed909b139d5eea01b5ed31a0
tobidev <samueloluwatobiloba48@gmail.com>
2023-08-20 12:58:32 +0100

    final touching

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,10 +14,12 @@ const Footer = () => {
             </Link>
 
             <div className="flex flex-wrap gap-y-[32px] gap-x-4 md:gap-[80px] text-black-black_8 dark:text-white-white_8 text-[14px]  lg:text-[28px] font-[300] underline ">
-              <Link href="">Behance</Link>
-              <Link href="">LinkedIn</Link>
-              <Link href="">Twitter</Link>
-              <Link href="">Dribbble</Link>
+              <Link href="www.behance.net%2FdenapomosesDMD">Behance</Link>
+              <Link href="www.linkedin.com%2Fin%2Fmosesdenapo%2F">
+                LinkedIn
+              </Link>
+              <Link href="twitter.com%2Fdammyboy121">Twitter</Link>
+              <Link href="dribbble.com%2FDammy_121">Dribbble</Link>
             </div>
           </div>
         </footer>
